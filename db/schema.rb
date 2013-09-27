17a4d355069743bcdded6f28e4b8a38d163f7f1c
Michael Allan <michaelallan@optushome.com.au>
2013-09-27 18:44:45 +1000

    rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130927082728) do
+ActiveRecord::Schema.define(version: 20130927084335) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,16 @@ ActiveRecord::Schema.define(version: 20130927082728) do
   end
 
   add_index "products", ["user_id"], name: "index_products_on_user_id", using: :btree
+
+  create_table "sales", force: true do |t|
+    t.string   "email"
+    t.string   "guid"
+    t.integer  "product_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sales", ["product_id"], name: "index_sales_on_product_id", using: :btree
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
