2e5e6783f5092d7415f3f830d44e0c0c65b9b8d5
Michael Allan <michaelallan@optushome.com.au>
2013-09-27 17:57:22 +1000

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sales::Application.config.secret_key_base = 'e1c946b3f98b83a6467c77c984e43b0d8354f7027cf6e9413759fc7a3389cf9797a178b5cef5e30b1bcd2066b96bdea16b5fd35982dc61667835637e8ced0c3f'
